6bd9f820fa86b3067dcb8851a442edf386f6f21d
Vadim <vad1mk4@bk.ru>
2023-11-19 13:08:55 +0300

    Making Tests.py

--- mountain/perevalapp/tests.py
+++ mountain/perevalapp/tests.py
@@ -1,3 +1,113 @@
+from django.urls import reverse
+from rest_framework import status
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Pereval, Coord, MyUser, Level
+from .serializers import PerevalSerializer
+
+
+class MountApiTestCase(APITestCase):
+
+    def setUp(self):
+        user_1 = MyUser.objects.create(email='Test_1', phone=1111, fam='Test_1', name='Test_1', otc='Test_1')
+        user_2 = MyUser.objects.create(email='Test_2', phone=2222, fam='Test_2', name='Test_2', otc='Test_2')
+        coords_1 = Coord.objects.create(latitude=5.0002, longitude=5.0002, height=100)
+        coords_2 = Coord.objects.create(latitude=5.0022, longitude=5.0022, height=200)
+        level_1 = Level.objects.create(winter='1a', spring='1a', summer='1a', autumn='1a')
+        level_2 = Level.objects.create(winter='2a', spring='2a', summer='2a', autumn='2a')
+        self.mount_1 = Pereval.objects.create(user_id=user_1, beauty_title='beauty_title_1', title="title_1",
+                                            other_titles='other_titles_1', coord_id=coords_1, level_id=level_1)
+        self.mount_2 = Pereval.objects.create(user_id=user_2, beauty_title='beauty_title_2', title="title_2",
+                                            other_titles='other_titles_2', coord_id=coords_2, level_id=level_2)
+
+    def test_get_list(self):
+        url = reverse('mount-list')
+        response = self.client.get(url)
+        serializer_data = PerevalSerializer([self.mount_1, self.mount_2], many=True).data
+        self.assertEqual(serializer_data, response.data)
+        self.assertEqual(len(serializer_data), 2)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_get_detail(self):
+        url = reverse('mount-detail', args=(self.mount_1.id,))
+        response = self.client.get(url)
+        serializer_data = PerevalSerializer(self.mount_1).data
+        self.assertEqual(serializer_data, response.data)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+
+class MountSerializerTestCase(TestCase):
+    def setUp(self):
+        user_1 = MyUser.objects.create(email="Test_1", phone=1111, fam="Test_1", name="Test_1", otc="Test_1")
+        user_2 = MyUser.objects.create(email="Test_2", phone=2222, fam="Test_2", name="Test_2", otc="Test_2")
+        coords_1 = Coord.objects.create(latitude=5.0002, longitude=5.0002, height=100)
+        coords_2 = Coord.objects.create(latitude=5.0022, longitude=5.0022, height=200)
+        level_1 = Level.objects.create(winter='1a', spring='1a', summer='1a', autumn='1a')
+        level_2 = Level.objects.create(winter='2a', spring='2a', summer='2a', autumn='2a')
+        self.mount_1 = Pereval.objects.create(user_id=user_1, beauty_title="beauty_title_1", title="title_1",
+                                            other_titles="other_titles_1",
+                                            coord_id=coords_1, level_id=level_1)
+        self.mount_2 = Pereval.objects.create(user_id=user_2, beauty_title="beauty_title_2", title="title_2",
+                                            other_titles="other_titles_2",
+                                            coord_id=coords_2, level_id=level_2)
+
+    def test_check(self):
+        serializer_data = PerevalSerializer([self.mount_1, self.mount_2], many=True).data
+
+        expected_data = [
+            {
+                "id": 1,
+                "beauty_title": "beauty_title_1",
+                "title": "title_1",
+                "other_titles": "other_titles_1",
+                "connect": None,
+                "level_id": {
+                    "winter": "1a",
+                    "summer": "1a",
+                    "autumn": "1a",
+                    "spring": "1a"
+                },
+                "user_id": {
+                    "email": "Test_1",
+                    "phone": "1111",
+                    "fam": "Test_1",
+                    "name": "Test_1",
+                    "otc": "Test_1"
+                },
+                "coord_id": {
+                    "latitude": 5.0002,
+                    "longitude": 5.0002,
+                    "height": 100
+                },
+                "images": []
+            },
+            {
+                "id": 2,
+                "beauty_title": "beauty_title_2",
+                "title": "title_2",
+                "other_titles": "other_titles_2",
+                "connect": None,
+                "level_id": {
+                    "winter": "2a",
+                    "summer": "2a",
+                    "autumn": "2a",
+                    "spring": "2a"
+                },
+                "user_id": {
+                    "email": "Test_2",
+                    "phone": "2222",
+                    "fam": "Test_2",
+                    "name": "Test_2",
+                    "otc": "Test_2"
+                },
+                "coord_id": {
+                    "latitude": 5.0022,
+                    "longitude": 5.0022,
+                    "height": 200
+                },
+                "images": []
+            }
+        ]
+
+        self.assertEqual(serializer_data, expected_data)
